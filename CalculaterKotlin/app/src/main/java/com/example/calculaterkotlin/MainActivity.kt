e61eef7f8d0943ce05b8caf9391cfa1812430372
fatiihvarol <fatihvrl38@gmail.com>
2023-03-18 23:12:03 +0300

    Android Simple Calculater

---
+++ CalculaterKotlin/app/src/main/java/com/example/calculaterkotlin/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.calculaterkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    fun button1(view: View){
+
+    }
+    fun button2(view: View){
+
+    }
+    fun button3(view: View){
+
+    }
+    fun button4(view: View){
+
+    }
+}
